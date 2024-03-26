b16d4318a14f3e618b21b6d2a658c9867b667873
Andrew.Alvand <halavehzadeh@outlook.com>
2024-03-26 17:54:23 -0400

    create first_ec2.tf file

---
+++ first_ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t3.micro"
+}

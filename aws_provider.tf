7da885ebedcd90483d7584392a1676d102f35932
Andrew.Alvand <halavehzadeh@outlook.com>
2024-03-27 10:23:36 -0400

    modified AWS provider and added AWS user resources

--- aws_provider.tf
+++ aws_provider.tf
@@ -4,10 +4,18 @@ provider "aws" {
   secret_key = "PUT-YOUR-SECRET-KEY-HERE"
 }
 
+#Create a Ec2 Intance in AWS
 resource "aws_instance" "myec2" {
   ami           = "ami-0c101f26f147fa7fd"
   instance_type = "t3.micro"
   tags = {
     Name = "My-First-Ec2"
   }
+}
+
+#Add a user to IAM service
+
+resource "aws_iam_user" "andrew" {
+  name = "kplabs-demo-user"
+
 }

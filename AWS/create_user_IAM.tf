9925899c5fff6e7f3e36bd87b8b082a6005a3018
Andrew Alvand <122839430+halavehzadeh@users.noreply.github.com>
2024-03-28 15:24:47 -0400

    Update create_user_IAM.tf

--- AWS/create_user_IAM.tf
+++ AWS/create_user_IAM.tf
@@ -1,12 +1,10 @@
-#Add a user to IAM service
-
-resource "aws_iam_user" "andrew" {
-  name = "kplabs-demo-user"
+provider "aws" {
+  region = "us-east-1"
 
 }
+
 #Add a user to IAM service
 
 resource "aws_iam_user" "andrew" {
   name = "kplabs-demo-user"
-
 }

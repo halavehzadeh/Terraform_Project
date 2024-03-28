85ce7fcc53b357e23090a0458c2d6238747aedc4
Andrew Alvand <122839430+halavehzadeh@users.noreply.github.com>
2024-03-28 15:23:52 -0400

    Update EC2_instance.tf

--- AWS/EC2_instance.tf
+++ AWS/EC2_instance.tf
@@ -1,9 +1,7 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
-  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
-}
+  region = "us-east-1"
 
+}
 #Create a Ec2 Intance in AWS
 resource "aws_instance" "myec2" {
   ami           = "ami-0c101f26f147fa7fd"

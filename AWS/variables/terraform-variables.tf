ea880b2a0fa7def38875834a3b371d4079576f2b
Andrew Alvand <122839430+halavehzadeh@users.noreply.github.com>
2024-04-02 16:58:46 -0400

    Update terraform-variables.tf
    added variable ports to the  files

--- AWS/variables/terraform-variables.tf
+++ AWS/variables/terraform-variables.tf
@@ -16,21 +16,21 @@ resource "aws_security_group" "variable_test" {
 resource "aws_vpc_security_group_ingress_rule" "app_port" {
   security_group_id = aws_security_group.variable_test.id
   cidr_ipv4         = var.vpn_ip
-  from_port         = 8080
+  from_port         = var.app_port
   ip_protocol       = "tcp"
-  to_port           = 8080
+  to_port           = var.app_port
 }
 resource "aws_vpc_security_group_ingress_rule" "aah_port" {
   security_group_id = aws_security_group.variable_test.id
   cidr_ipv4         = var.vpn_ip
-  from_port         = 22
+  from_port         = var.ssh_port
   ip_protocol       = "tcp"
-  to_port           = 22
+  to_port           = var.ssh_port
 }
 resource "aws_vpc_security_group_ingress_rule" "ftp_port" {
   security_group_id = aws_security_group.variable_test.id
   cidr_ipv4         = var.vpn_ip
-  from_port         = 21
+  from_port         = var.ftp_port
   ip_protocol       = "tcp"
-  to_port           = 21
+  to_port           = var.ftp_port
 }

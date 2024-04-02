de0f51073b02502ca5d632f0d9200b89891908a7
Andrew Alvand <122839430+halavehzadeh@users.noreply.github.com>
2024-04-02 16:57:06 -0400

    Update central-location.tf
    added sources port to the files

--- AWS/variables/central-location.tf
+++ AWS/variables/central-location.tf
@@ -1,3 +1,14 @@
 variable "vpn_ip" {
   default = "200.7.124.210/32"
 }
+variable "app_port" {
+  default = "8080"
+}
+
+variable "shh_port" {
+  default = "22"
+}
+
+variable "ftp_port" {
+  default = "21"
+}
